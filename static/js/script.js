622ea1aaa552b5643e56d8117cbae16dd45b624d
salissadiq <salissadiq@gmail.com>
2022-07-21 15:40:21 +0100

    Delete the entire code base and rewrite everything from scratch

--- static/js/script.js
+++ static/js/script.js
@@ -1,16 +1,14 @@
 function ageInDays() {
-
-    var birthYear = prompt("What year were you born ..... good friend?");
-    var currentYear = new Date().getFullYear();
-    var daysInYear = 365;
-    var ageInDays = (currentYear - birthYear) * daysInYear;
-
-    var newH1 = document.createElement('h1');
-    var textResult = document.createTextNode('You\'re ' + ageInDays + ' age in days')
-    newH1.setAttribute('id', 'ageInDays');
-    newH1.appendChild(textResult);
-    document.getElementById('flex-col-result').appendChild(newH1);
-
+    var birthYear = prompt("What year were you born ? .... good friend")
+    var currentYear = new Date().getFullYear()
+    var daysInYear = 365
+    var daysOld = (currentYear - birthYear) * daysInYear
+    
+    var newH1 = document.createElement("h1")
+    var textResult = document.createTextNode(' You\'re ' + daysOld + ' days age old')
+    newH1.setAttribute('id', 'ageInDays')
+    newH1.appendChild(textResult)
+    document.getElementById('flex-box-result-container').appendChild(newH1)
 }
 
 function reset() {
